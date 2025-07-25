5f27504676470fc5727fdda245f27b8cbf7200fe
Karbid Dev <karbid.dev@gmail.com>
2025-07-25 15:25:03 +0700

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,3 +1,5 @@
 -- Load Universal Script
 loadstring(game:HttpGet("https://raw.githubusercontent.com/karbid-dev/Karbid/main/universal.lua"))()
 
+-- Load Game Script (Example: GameA)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/karbid-dev/Karbid/main/Games/Game_A.lua"))()
