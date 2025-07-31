3bd4481f02311f1d731887be7e3a63524ef06a2b
Karbid Dev <karbid.dev@gmail.com>
2025-07-31 11:09:32 +0700

    Update and rename Main.lua to zpp0kogh0t

--- Main.lua
+++
@@ -1,6 +0,0 @@
--- Load Universal Script
-loadstring(game:HttpGet("https://raw.githubusercontent.com/karbid-dev/Karbid/main/universal.lua"))()
-
-
--- Load Game Script (Example: GameA)
-loadstring(game:HttpGet("https://raw.githubusercontent.com/karbid-dev/Karbid/main/Games/Game_A.lua"))()
